700de7392adabb8d7ccd2604ebafc402ae69d880
tam nguyen <ndtam.273@gmail.com>
2021-01-13 17:55:54 +0700

    keep coding

--- Dating Login/ContentView.swift
+++ Dating Login/ContentView.swift
@@ -31,7 +31,10 @@ struct Home: View {
                 .frame(width: 200, height: 180)
             HStack{
                 Button(action: {
-                    self.index = 0
+                    withAnimation(Animation.spring(response: 0.8, dampingFraction: 0.5, blendDuration: 0.5)) {
+                        self.index = 0
+                    }
+                    
                 }){
                     Text("Existing")
                         .foregroundColor(self.index == 0 ? .black : .white)
@@ -42,7 +45,9 @@ struct Home: View {
                 .clipShape(Capsule())
                 
                 Button(action: {
-                    self.index = 1
+                    withAnimation(Animation.spring(response: 0.8, dampingFraction: 0.5, blendDuration: 0.5)) {
+                        self.index = 1
+                    }
                 }){
                     Text("New")
                         .foregroundColor(self.index == 1 ? .black : .white)
@@ -54,7 +59,24 @@ struct Home: View {
             }.background(Color.black.opacity(0.1))
             .clipShape(Capsule())
             .padding(.top, 25)
-            Login()
+            
+            if self.index == 1 {
+                SignUp()
+            } else {
+                Login()
+            }
+            
+            Button(action: {
+                
+            }) {
+                Text("Forgot Password?")
+                    .foregroundColor(.white)
+            }
+            
+            HStack {
+                
+            }
+            
         }
         .padding()
     }
@@ -65,35 +87,120 @@ struct Login: View {
     @State var pass = ""
     var body: some View {
         VStack {
-            HStack(spacing: 15){
-                Image(systemName: "envelope")
-                    .foregroundColor(.black)
-                TextField("Enter Email Address", text: self.$mail)
-            }.padding(.vertical, 20)
-            
-            Divider()
-            
-            HStack(spacing: 20){
-                Image(systemName: "lock")
-                    .resizable()
-                    .frame(width: 15, height: 18)
-                    .foregroundColor(.black)
-                SecureField("Enter Password", text: self.$pass)
-                Button(action: {
-                    
-                }) {
-                    Image(systemName: "eye")
+            VStack {
+                HStack(spacing: 15){
+                    Image(systemName: "envelope")
                         .foregroundColor(.black)
-                }
-            }.padding(.vertical, 20)
+                    TextField("Enter Email Address", text: self.$mail)
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 20){
+                    Image(systemName: "lock")
+                        .resizable()
+                        .frame(width: 15, height: 18)
+                        .foregroundColor(.black)
+                    SecureField("Enter Password", text: self.$pass)
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "eye")
+                            .foregroundColor(.black)
+                    }
+                }.padding(.vertical, 20)
+                
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 20)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.top, 25)
             
-          
-            
+            Button(action:  {}) {
+                Text("LOGIN")
+                    .foregroundColor(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                    .background(LinearGradient(gradient: .init(colors: [Color("Color2"), Color("Color1"), Color("Color")]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(8)
+                    .offset(y: -40)
+                    .padding(.bottom, -40)
+                    .shadow(radius: 15)
+            }
             
         }
-        .padding(.vertical)
-        .padding(.horizontal, 20)
-        .background(Color.white)
-        .cornerRadius(10)
+    }
+}
+
+struct SignUp: View {
+    @State var mail = ""
+    @State var pass = ""
+    @State var repass = ""
+    var body: some View {
+        VStack {
+            VStack {
+                HStack(spacing: 15){
+                    Image(systemName: "envelope")
+                        .foregroundColor(.black)
+                    TextField("Enter Email Address", text: self.$mail)
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 20){
+                    Image(systemName: "lock")
+                        .resizable()
+                        .frame(width: 15, height: 18)
+                        .foregroundColor(.black)
+                    SecureField("Enter Password", text: self.$pass)
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "eye")
+                            .foregroundColor(.black)
+                    }
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 20){
+                    Image(systemName: "lock")
+                        .resizable()
+                        .frame(width: 15, height: 18)
+                        .foregroundColor(.black)
+                    SecureField("Re-Enter", text: self.$repass)
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "eye")
+                            .foregroundColor(.black)
+                    }
+                }.padding(.vertical, 20)
+                
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 20)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.top, 25)
+            
+            Button(action:  {}) {
+                Text("SIGNUP")
+                    .foregroundColor(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                    .background(LinearGradient(gradient: .init(colors: [Color("Color2"), Color("Color1"), Color("Color")]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(8)
+                    .offset(y: -40)
+                    .padding(.bottom, -40)
+                    .shadow(radius: 25)
+            }
+            
+        }
     }
 }
